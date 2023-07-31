b3b7fde04610feb8470d630ef45855269722603b
Antunes Gabriel <gabrielantunescontato@gmail.com>
2023-07-31 13:58:03 -0300

    feat: add user table

--- scripts/sql/migrations/20230731165642_create_users_table.sql
+++ scripts/sql/migrations/20230731165642_create_users_table.sql
@@ -8,12 +8,5 @@ CREATE TABLE users (
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP
 );
--- +goose StatementBegin
-SELECT 'up SQL query';
--- +goose StatementEnd
-
 -- +goose Down
 DROP TABLE users;
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
