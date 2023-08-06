ed86eb2ecd2bb630b1929ed3b87143c17cda1d03
Antunes Gabriel <gabrielantunescontato@gmail.com>
2023-08-05 21:50:03 -0300

    test: adding health unit test

--- internal/interfaces/api/controller/check_health_controller.go
+++ internal/interfaces/api/controller/check_health_controller.go
@@ -25,7 +25,7 @@ func (it *CheckHealthController) Handle(w http.ResponseWriter, _ *http.Request) 
 	w.Header().Set("Content-Type", "application/json")
 
 	if err := it.usecase.Execute(); err != nil {
-		w.WriteHeader(http.StatusFailedDependency)
+		w.WriteHeader(http.StatusInternalServerError)
 
 		output := dto.HealthCheckOutput{
 			Up: false,
--- internal/interfaces/api/controller/check_health_controller_test.go
+++ internal/interfaces/api/controller/check_health_controller_test.go
@@ -1,9 +1,101 @@
 package controller
 
-import "testing"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/antunesgabriel/gopher-template-default/internal/application/usecase"
+	m "github.com/antunesgabriel/gopher-template-default/test/mock"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestCheckHealthController(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+func TestCheckHealthController_Handle(t *testing.T) {
+	t.Run("it should response up true if db is up", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/health", nil)
 
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+
+		usecaseDBUp := newMockUseCase(nil)
+		controller := NewCheckHealthController(usecaseDBUp)
+
+		controller.Handle(rr, req)
+
+		response := controllerResponse{}
+
+		err = json.NewDecoder(rr.Body).Decode(&response)
+
+		if err != nil {
+			t.Errorf("got %s want %s", err, "no error")
+		}
+
+		if rr.Code != http.StatusOK {
+			t.Errorf("got %d want %d", rr.Code, http.StatusOK)
+		}
+
+		if response.Error != "" {
+			t.Errorf("got %s want %s", response.Error, "error should to be empty")
+		}
+
+		if response.Data.Up != true {
+			t.Errorf("got %t want %t", response.Data.Up, true)
+		}
 	})
+
+	t.Run("it should response up false if db is down", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/health", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+
+		response := controllerResponse{}
+
+		usecaseDBDown := newMockUseCase(errors.New("db_down"))
+		controller := NewCheckHealthController(usecaseDBDown)
+
+		controller.Handle(rr, req)
+
+		err = json.NewDecoder(rr.Body).Decode(&response)
+
+		if err != nil {
+			t.Errorf("got %s want %s", err, "no error")
+		}
+
+		if rr.Code != http.StatusInternalServerError {
+			t.Errorf("got %d want %d", rr.Code, http.StatusInternalServerError)
+		}
+
+		if response.Error == "" {
+			t.Errorf("got %s want %s", response.Error, "db_down")
+		}
+
+		if response.Data.Up != false {
+			t.Errorf("got %t want %t", response.Data.Up, false)
+		}
+	})
+}
+
+type upField struct {
+	Up bool `json:"up"`
+}
+
+type controllerResponse struct {
+	Error string  `json:"error,omitempty"`
+	Data  upField `json:"data,omitempty"`
+}
+
+func newMockUseCase(err error) *usecase.CheckHealthUseCase {
+	r := m.MockHealthRepository{
+		Return: err,
+	}
+
+	u := usecase.NewCheckHealthUseCase(&r)
+
+	return u
 }
